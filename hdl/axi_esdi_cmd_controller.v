bd0ddeda3c98afdee4c46de803ed7721583ebcaa
Chris Simmons <cw@cfsimmons.com>
2024-03-03 15:57:49 -0600

    Work on serial command interface

--- hdl/axi_esdi_cmd_controller.v
+++ hdl/axi_esdi_cmd_controller.v
@@ -31,8 +31,8 @@ module axi_esdi_cmd_controller #(
     output reg [31:0] csr_rdata,
     output reg [1:0] csr_rresp,
 
-    output esdi_transfer_req,
-    output esdi_command_data,
+    output reg esdi_transfer_req,
+    output reg esdi_command_data,
     input esdi_transfer_ack,
     input esdi_confstat_data,
     input esdi_command_complete,
@@ -50,10 +50,28 @@ module axi_esdi_cmd_controller #(
 
     reg [31:0] control_register;
 
+    reg buffered_data_out_valid;
+    reg [31:0] buffered_data_out;
+
+    reg buffered_data_in_valid;
+    reg [31:0] buffered_data_in;
+
+    reg [2:0] state;
+    reg reading;
+    reg [5:0] bit_count;
+    reg [31:0] cycle_count;
+    reg [16:0] data_out;    // bit 0 is odd parity bit
+    reg is_query;           // 0 = command, 1 = configuration/status transfer
+    reg [16:0] data_in;
+
     always @(posedge csr_aclk)
     begin
         if (!csr_aresetn)
         begin
+            esdi_transfer_req <= 1;
+            esdi_command_data <= 1;
+            state <= 0;
+
             write_addr_valid <= 0;
             write_data_valid <= 0;
             csr_bvalid <= 0;
@@ -61,6 +79,135 @@ module axi_esdi_cmd_controller #(
         end
         else
         begin
+
+            /* Serial Processing */
+
+            cycle_count <= cycle_count + 1;
+
+            // Wait for data to send
+            if (state == 0)
+            begin
+                if (buffered_data_out_valid)
+                begin
+                    buffered_data_out_valid <= 0;
+                    data_out <= {buffered_data_out[15:0], ~^buffered_data_out[15:0]};
+                    is_query <= buffered_data_out[16];
+                    state <= 1;
+                    reading <= 0;
+                    bit_count <= 0;
+                    cycle_count <= 0;
+                end
+            end
+            // Send a bit of data
+            else if (state == 1)
+            begin
+                
+                if (cycle_count == 0)
+                begin
+                    if (!reading)
+                    begin
+                        esdi_command_data <= !data_out[16];
+                        data_out <= data_out << 1;
+                    end
+                    bit_count <= bit_count + 1;
+                end
+                if (cycle_count == DATA_SETUP)
+                begin
+                    esdi_transfer_req <= 0;
+                    state <= 2;
+                    cycle_count <= 0;
+                end
+            end
+            // Wait for Ack
+            else if (state == 2)
+            begin
+
+                if (!esdi_transfer_ack)
+                begin
+                    state <= 3;
+                    cycle_count <= 0;
+                    if (reading)
+                    begin
+                        data_in <= {data_in[15:0], !esdi_confstat_data};
+                    end
+                end
+
+                if (cycle_count == BIT_TIMEOUT)
+                begin
+                    state <= 0;
+                    if (is_query)
+                    begin
+                        buffered_data_in_valid <= 1;
+                        buffered_data_in <= {15'h1, 17'h0};
+                    end
+                end
+            end
+            // Wait some time to deassert req
+            else if (state == 3)
+            begin
+
+                if (cycle_count == ACK_TO_NREQ)
+                begin
+                    esdi_transfer_req <= 1;
+                    state <= 4;
+                    cycle_count <= 0;
+                end
+            end
+            // Wait for Ack deassert
+            else if (state == 4)
+            begin
+
+                if (esdi_transfer_ack)
+                begin
+
+                    if (bit_count == 17)
+                    begin
+                        if (is_query)
+                        begin
+                            if (!reading)
+                            begin
+                                // Go back to transfering bit 0, but this time reading instead of writing
+                                state <= 1;
+                                reading <= 1;
+                                bit_count <= 0;
+                                cycle_count <= 0;
+                            end
+                            else
+                            begin
+                                // We've read back 17 bits, so return the results to the user
+                                state <= 0;
+                                buffered_data_in_valid <= 1;
+                                buffered_data_in <= {15'h0, (~^data_in[16:1] != data_in[0]), data_in[16:1]};
+                            end
+                        end
+                        else
+                        begin
+                            // We've finished sending 17 bits of a command, return to idle state
+                            state <= 0;
+                        end
+
+                    end
+                    else
+                    begin
+                        // We have more bits to process
+                        state <= 1;
+                        cycle_count <= 0;
+                    end
+
+                end
+
+                if (cycle_count == BIT_TIMEOUT)
+                begin
+                    state <= 0;
+                    if (is_query)
+                    begin
+                        buffered_data_in_valid <= 1;
+                        buffered_data_in <= {15'h1, 17'h0};
+                    end
+                end
+            end
+
+            /* Register Interface*/
 
             if (csr_bready)
                 csr_bvalid <= 0;
@@ -87,6 +234,10 @@ module axi_esdi_cmd_controller #(
 
                 case (write_addr[4:2])
                     0 : control_register <= write_data;
+                    1 : begin
+                        buffered_data_out_valid <= 1;
+                        buffered_data_out <= write_data;
+                    end
                 endcase
 
                 csr_bvalid <= 1;
@@ -97,7 +248,11 @@ module axi_esdi_cmd_controller #(
             begin
 
                 case (csr_araddr[4:2])
-                    0 : csr_rdata <= control_register;
+                    0 : csr_rdata <= {30'h0, buffered_data_in_valid, buffered_data_out_valid};
+                    1 : begin
+                        csr_rdata <= buffered_data_in;
+                        buffered_data_in_valid <= 0;
+                    end
                 endcase
 
                 csr_rvalid <= 1;
