532e0f636bd720bc4feb3dd3a223a97cb72a36e3
Christopher Simmons <cw@cfsimmons.com>
2024-08-06 00:55:26 -0500

    Update find.py

--- software/find.py
+++ software/find.py
@@ -1,14 +1,22 @@
+
+# Use this tool to find a CRC when some of your data might be corrupted
+
 from subprocess import check_output
 import itertools
 import glob
+import sys
 
-files = glob.glob("s_*.bin")
+if len(sys.argv) != 3:
+    print("Usage: python find.py <glob> <crc width>")
+    print("Example: python find.py ./mydataset/s*.bin 32")
+else:
+    files = glob.glob(sys.argv[1])
 
-for subset in itertools.combinations(files, 4):
+    for subset in itertools.combinations(files, 4):
 
-    command_str = "reveng.exe -s -w 32 -f %s" % (" ".join(subset))
-    print(command_str)
-    try:
-        print(check_output(command_str, shell=True).decode("utf-8"))
-    except:
-        pass
+        command_str = "reveng.exe -s -w %s -f %s" % (sys.argv[2], " ".join(subset))
+        print(command_str)
+        try:
+            print(check_output(command_str, shell=True).decode("utf-8"))
+        except:
+            pass
