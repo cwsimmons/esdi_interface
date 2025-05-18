9c23e61301b5a9f8ee7bbf3ff31bfc9d9f6e4d07
Christopher Simmons <cw@cfsimmons.com>
2025-05-18 18:20:39 -0500

    Remove dcp from tcl

--- esdi_interface.tcl
+++ esdi_interface.tcl
@@ -39,15 +39,6 @@
 # Check file required for this script exists
 proc checkRequiredFiles { origin_dir} {
   set status true
-  set files [list \
- "[file normalize "$origin_dir/esdi_interface/esdi_interface.srcs/utils_1/imports/synth_1/top.dcp"]"\
-  ]
-  foreach ifile $files {
-    if { ![file isfile $ifile] } {
-      puts " Could not find local file $ifile "
-      set status false
-    }
-  }
 
   set files [list \
  "[file normalize "$origin_dir/hdl/axi_esdi_cmd_controller.v"]"\
@@ -234,20 +225,6 @@ set_property -name "top_lib" -value "xil_defaultlib" -objects $obj
 
 # Set 'utils_1' fileset object
 set obj [get_filesets utils_1]
-# Import local files from the original project
-set files [list \
- [file normalize "${origin_dir}/esdi_interface/esdi_interface.srcs/utils_1/imports/synth_1/top.dcp" ]\
-]
-set imported_files [import_files -fileset utils_1 $files]
-
-# Set 'utils_1' fileset file properties for remote files
-# None
-
-# Set 'utils_1' fileset file properties for local files
-set file "synth_1/top.dcp"
-set file_obj [get_files -of_objects [get_filesets utils_1] [list "*$file"]]
-set_property -name "netlist_only" -value "0" -objects $file_obj
-
 
 # Set 'utils_1' fileset properties
 set obj [get_filesets utils_1]
@@ -898,7 +875,6 @@ if { $obj != "" } {
 
 }
 set obj [get_runs synth_1]
-set_property -name "incremental_checkpoint" -value "$proj_dir/esdi_interface.srcs/utils_1/imports/synth_1/top.dcp" -objects $obj
 set_property -name "auto_incremental_checkpoint" -value "1" -objects $obj
 set_property -name "strategy" -value "Vivado Synthesis Defaults" -objects $obj
 
